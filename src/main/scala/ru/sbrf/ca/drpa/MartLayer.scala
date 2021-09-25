f4198bf9b77f4fb425c25cef9d5c50176be790fa
Rhilda <ildariismagilov@mail.ru>
2021-09-25 20:59:14 +0300

    fix last tables

--- src/main/scala/ru/sbrf/ca/drpa/MartLayer.scala
+++ src/main/scala/ru/sbrf/ca/drpa/MartLayer.scala
@@ -15,11 +15,10 @@ object MartLayer {
     val vaccines_by_freedom = main.select(col("people_fully_vaccinated_per_hundred"), col("Freedom_to_make_life_choices")).sort(col("Freedom_to_make_life_choices"))
     val vaccines_by_corruption = main.select(col("people_fully_vaccinated_per_hundred"), col("Perceptions_of_corruption")).sort(col("Perceptions_of_corruption"))
 
-    val error_vaccines_country = session.sql("SELECT * FROM happiness_2019 where Country_or_region not in (select xcountry from country_vaccinations)")
-    val error_happiness_country = session.sql("SELECT * FROM country_vaccinations where xcountry not in (select Country_or_region from happiness_2019)")
+    val error_vaccines_country = session.sql("SELECT distinct Country_or_region FROM happiness_2019 where Country_or_region not in (select xcountry from country_vaccinations)")
+    val error_happiness_country = session.sql("SELECT distinct xcountry FROM country_vaccinations where xcountry not in (select Country_or_region from happiness_2019)")
 
     val country_vaccinations_by_manufacturer = session.sql("SELECT * FROM country_vaccinations_by_manufacturer")
-
     val country_date_window_spec = Window.partitionBy("location").orderBy(col("date").desc)
     val latest_country_vaccinations_by_manufacturer =
       country_vaccinations_by_manufacturer
