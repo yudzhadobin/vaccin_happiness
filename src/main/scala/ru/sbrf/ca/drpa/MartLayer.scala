d48a31aee50dd0d4b1d3d70465b5862a678f64d4
Rhilda <ildariismagilov@mail.ru>
2021-09-25 14:36:19 +0300

    make all tables

--- src/main/scala/ru/sbrf/ca/drpa/MartLayer.scala
+++ src/main/scala/ru/sbrf/ca/drpa/MartLayer.scala
@@ -8,13 +8,16 @@ object MartLayer {
 
   def run()(implicit session: SparkSession): Unit = {
     val main = session.sql("SELECT * FROM happiness_2019 h join country_vaccinations cv on h.Country_or_region = cv.xcountry")
+
     val vaccines_by_rank = main.select(col("people_fully_vaccinated_per_hundred"), col("Overall_rank")).sort(col("Overall_rank"))
     val vaccines_by_gdp = main.select(col("people_fully_vaccinated_per_hundred"), col("GDP_per_capita")).sort(col("GDP_per_capita"))
     val vaccines_by_healthy = main.select(col("people_fully_vaccinated_per_hundred"), col("Healthy_life_expectancy")).sort(col("Healthy_life_expectancy"))
     val vaccines_by_freedom = main.select(col("people_fully_vaccinated_per_hundred"), col("Freedom_to_make_life_choices")).sort(col("Freedom_to_make_life_choices"))
     val vaccines_by_corruption = main.select(col("people_fully_vaccinated_per_hundred"), col("Perceptions_of_corruption")).sort(col("Perceptions_of_corruption"))
 
-    val country_vaccinations = session.sql("SELECT * FROM country_vaccinations")
+    val error_vaccines_country = session.sql("SELECT * FROM happiness_2019 where Country_or_region not in (select xcountry from country_vaccinations)")
+    val error_happiness_country = session.sql("SELECT * FROM country_vaccinations where xcountry not in (select Country_or_region from happiness_2019)")
+
     val country_vaccinations_by_manufacturer = session.sql("SELECT * FROM country_vaccinations_by_manufacturer")
 
     val country_date_window_spec = Window.partitionBy("location").orderBy(col("date").desc)
